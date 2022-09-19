5fe1955e5a6c3b568a711f56f51d53887181f924
Jranae <112411872+Jranae@users.noreply.github.com>
2022-09-19 18:21:53 -0400

    Add files via upload

---
+++ AutoExplicit.java
@@ -0,0 +1,19 @@
+
+public class AutoExplicit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+int i = 30;
+
+		double d = i;
+		float f = (float)d;
+		int i1 = (int)f;
+		System.out.println("The integer number is " + i);
+		System.out.println("The double number is " + d);
+		System.out.println("The floating point number is " + f);
+		System.out.println("The integer number is " + i1);
+		
+	}
+
+}
