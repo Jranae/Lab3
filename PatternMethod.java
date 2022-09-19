245b2e8448a37b288f78809e475806ee407d231c
Jranae <112411872+Jranae@users.noreply.github.com>
2022-09-19 18:16:10 -0400

    Add files via upload

---
+++ PatternMethod.java
@@ -0,0 +1,18 @@
+
+public class PatternMethod {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	System.out.println("       *     ");
+	System.out.println("      ***     ");
+	System.out.println("     *****    ");
+	System.out.println("    *******     ");
+	System.out.println("   *********     ");
+	System.out.println("    *******    ");
+	System.out.println("     *****    ");
+	System.out.println("      ***   ");
+	System.out.println("       *     ");
+	
+	}
+
+}
